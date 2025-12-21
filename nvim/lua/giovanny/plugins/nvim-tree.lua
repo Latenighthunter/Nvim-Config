04d4b9ef2d892363df21b73fc7c49450544d1344
Giovanny <gm.giovannymartinez@gmail.com>
2025-12-21 14:51:51 -0600

    added telescope

--- nvim/lua/giovanny/plugins/nvim-tree.lua
+++ nvim/lua/giovanny/plugins/nvim-tree.lua
@@ -2,49 +2,24 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    local nvimtree = require("nvim-tree")
-
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
+    require("nvim-tree").setup({
+      sort = {
+        sorter = "case_sensitive",
+      },
       view = {
-        width = 50,
-        relativenumber = true,
+        width = 25,
       },
-      -- change folder arrow icons
       renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
+        group_empty = true,
       },
       filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
+        dotfiles = true,
       },
     })
-
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
--- nvim/lua/giovanny/plugins/treesitter.lua
+++ nvim/lua/giovanny/plugins/treesitter.lua
@@ -1,10 +1,18 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
+  build = ':TSUpdate',
   config = function()
-    require'nvim-treesitter'.setup({
-      ensure_installed = {},      -- or remove this entirely
-      auto_install = false,
+    require('nvim-treesitter').setup({
+      ensure_installed = { "lua", "python", "javascript", "c", "cpp", "html", "css", "javascript", "typescript", "tex" }, -- add languages you want
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
     })
   end
 }
