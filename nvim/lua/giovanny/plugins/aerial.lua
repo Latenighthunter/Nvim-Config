0b893e063266cf5b55f441c56697c0b1a35e77ac
Giovanny <gm.giovannymartinez@gmail.com>
2025-12-25 21:37:54 -0600

    fixed nvim tree and aerial

--- nvim/lua/giovanny/plugins/aerial.lua
+++ nvim/lua/giovanny/plugins/aerial.lua
@@ -5,12 +5,12 @@ return {
   dependencies = {
      "nvim-treesitter/nvim-treesitter",
      "nvim-tree/nvim-web-devicons"
-  },
-  
+  }, 
   config = function()
     require("aerial").setup({
     -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-    filter_kind = false,
+    filter_kind = nil,
+    backends = { "treesitter", "lsp"},
     on_attach = function(bufnr)
     -- Jump forwards/backwards with '{' and '}'
     vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
